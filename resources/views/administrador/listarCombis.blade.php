b331ea0df25cf35e41465b440529e9964477395c
Bemyi <benfrecce2012@gmail.com>
2021-05-02 19:08:00 -0300

    cartelitos

--- resources/views/administrador/listarCombis.blade.php
+++ resources/views/administrador/listarCombis.blade.php
@@ -40,6 +40,12 @@
 							</tr>
 						</tbody>
 					</table>
+					@if(Session::has('message'))
+					<div class="alert alert-danger alert-dismissible" role="alert">
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						{{Session::get('message')}}
+					</div>
+					@endif
 					<a href="{{route('combi19.altaCombi')}}">Alta combi</a>
 					{{$combis->links()}}
 				</div>
