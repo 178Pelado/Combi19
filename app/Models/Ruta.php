4fe8f90208619afe822550839bc6a58aed00fc90
Bemyi <benfrecce2012@gmail.com>
2021-05-06 10:55:42 -0300

    modificar combi

--- app/Models/Ruta.php
+++ app/Models/Ruta.php
@@ -9,25 +9,24 @@ use \Askedio\SoftCascade\Traits\SoftCascadeTrait;
 
 class Ruta extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
-    use SoftCascadeTrait;
-    protected $dates = ['deleted_at'];
-    protected $table = "rutas";
+  use HasFactory;
+  use SoftDeletes;
+  use SoftCascadeTrait;
+  protected $dates = ['deleted_at'];
+  protected $table = "rutas";
 
-    protected $fillable = [
-      'distancia_km',
-        'origen_id',
+  protected $fillable = [
+    'distancia_km',
+    'origen_id',
+    'descripcion',
+    'destino_id',
+  ];
 
-        'descripcion',
-        'destino_id',
-    ];
+  public function origen(){
+    return $this->belongsTo('App\Models\Lugar', 'origen_id');
+  }
 
-    public function origen(){
-      return $this->belongsTo('App\Models\Lugar', 'origen_id');
-    }
-
-    public function destino(){
-      return $this->belongsTo('App\Models\Lugar', 'destino_id');
-    }
+  public function destino(){
+    return $this->belongsTo('App\Models\Lugar', 'destino_id');
+  }
 }
